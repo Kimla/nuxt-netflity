1b85fc2feeb0f746997419d154f7961823372595
Kim Larsson <kimla89@gmail.com>
2018-03-30 22:10:28 +0200

    Trying some async data

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,40 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-netlify
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div class="posts">
+        <div class="post" v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.body }}</p>
+        </div>
     </div>
-  </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import axios from 'axios';
 
 export default {
-  components: {
-    AppLogo
-  }
+    async asyncData () {
+        let res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+        return {
+            posts: res.data
+        }
+    },
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+body {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    color: #35495e;
+    letter-spacing: 1px;
+    padding: 15px;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.posts {
+    width: 800px;
+    margin: 0 auto;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.post {
+    margin-bottom: 30px;
 }
 </style>
